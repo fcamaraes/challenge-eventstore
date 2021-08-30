ae748973475b6597865a1f445280770944bb5181
fcamaraes <fcamaraes@gmail.com>
2021-08-30 00:22:33 -0300

    Add id attribute to Event class

--- src/main/java/net/intelie/challenges/Event.java
+++ src/main/java/net/intelie/challenges/Event.java
@@ -4,12 +4,18 @@ package net.intelie.challenges;
  * This is just an event stub, feel free to expand it if needed.
  */
 public class Event {
+    private final String id;
     private final String type;
     private final long timestamp;
 
-    public Event(String type, long timestamp) {
+    public Event(String id, String type, long timestamp) {
         this.type = type;
+        this.id = id;
         this.timestamp = timestamp;
+    }
+
+    public String id() {
+        return id;
     }
 
     public String type() {
@@ -19,4 +25,5 @@ public class Event {
     public long timestamp() {
         return timestamp;
     }
+
 }
